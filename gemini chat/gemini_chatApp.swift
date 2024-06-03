579cfd8523224b54bb21e5213073415b0f8b61ea
Reza Abdullah <bsse1335@iit.du.ac.bd>
2024-06-03 21:13:57 +0600

    Initial Commit

---
+++ gemini chat/gemini_chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  gemini_chatApp.swift
+//  gemini chat
+//
+//  Created by Md. Rezaul karim on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct gemini_chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ gemini chatUITests/gemini_chatUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  gemini_chatUITestsLaunchTests.swift
+//  gemini chatUITests
+//
+//  Created by Md. Rezaul karim on 3/6/24.
+//
+
+import XCTest
+
+final class gemini_chatUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
